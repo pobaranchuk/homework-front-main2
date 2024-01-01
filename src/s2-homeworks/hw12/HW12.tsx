66738e753fa6e07e1b3ab1553de19612d9795157
VPobaranchuk <pobaranchukv@gmail.com>
2024-01-01 15:19:44 +0100

    HW12 done

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -3,7 +3,7 @@ import s from './HW12.module.css'
 import s2 from '../../s1-main/App.module.css'
 import SuperSelect from '../hw07/common/c5-SuperSelect/SuperSelect'
 import {useDispatch, useSelector} from 'react-redux'
-import {ActionType, changeThemeId} from './bll/themeReducer'
+import {changeThemeId} from './bll/themeReducer'
 import {AppStoreType} from "../hw10/bll/store";
 
 /*
